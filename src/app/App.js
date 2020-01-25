17d0210932983f9a0e4aa2dffa5ab8fbab0a3fa6
Lasantha Basnayake <lasantha57@gmail.com>
2020-01-25 20:31:50 +0800

    Change shared components structure

--- src/app/App.js
+++ src/app/App.js
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-d
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 
-import ErrorHandler from '../shared/ErrorHandler';
-import Footer from '../shared/Footer';
-import Header from '../shared/Header';
+import ErrorHandler from './shared/ErrorHandler';
+import Footer from './shared/Footer';
+import Header from './shared/Header';
 import Flights from './flights/Flights';
 
 const App = () => {
--- src/app/shared/ErrorHandler.jsx
+++ src/app/shared/ErrorHandler.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { logError } from '../utils/logger';
+import { logError } from '../../utils/logger';
 
 class ErrorHandler extends Component {
     constructor(props) {
