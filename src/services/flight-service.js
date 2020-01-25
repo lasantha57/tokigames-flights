e5f0c8fb2f2fff8c646c1caab89994f6ebb2c90e
Lasantha Basnayake <lasantha57@gmail.com>
2020-01-25 16:50:06 +0800

    Aggregate flight services

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -1,11 +1,57 @@
 
 import axios from 'axios';
 
+const mapBusinessFlightData = (data) => {
+
+    const flightsData = [];
+    for (let i = 0; i < data.length; i++) {
+        const element = data[i];
+
+        const departure = new Date(element.departureTime);
+        const arrival = new Date(element.arrivalTime);
+
+        flightsData.push({
+            departure: element.departure,
+            arrival: element.arrival,
+            departureDate: departure.toDateString(),
+            departureTime: departure.toTimeString(),
+            arrivalDate: arrival.toDateString(),
+            arrivalTime: arrival.toTimeString(),
+        });
+    }
+
+    return flightsData;
+}
+
+const mapCheapFlightData = (data) => {
+    const flightsData = [];
+    for (let i = 0; i < data.length; i++) {
+        const element = data[i];
+        const arrival = new Date(element.arrival);
+        const departure = new Date(element.departure);
+
+        const routes = element.route ? element.route.split('-') : [];
+
+        flightsData.push({
+            departure: routes[0],
+            arrival: routes[1],
+            departureDate: departure.toDateString(),
+            departureTime: departure.toTimeString(),
+            arrivalDate: arrival.toDateString(),
+            arrivalTime: arrival.toTimeString(),
+        });
+    }
+
+    return flightsData;
+}
+
 const getAll = async () => {
     const [cheapFlights, businessFlights] = await axios.all([getCheapFlights(), getBusinessFlights()]);
-    //console.log([...cheapFlights.data.data, ...businessFlights.data.data])
-    //const flightData = mapFlightData();
-    return [...cheapFlights.data.data, ...businessFlights.data.data];
+    const cheapFlightsData = mapCheapFlightData(cheapFlights.data.data);
+    const businessFlightsData = mapBusinessFlightData(businessFlights.data.data);
+    const flights = [...cheapFlightsData, ...businessFlightsData]
+    flights.forEach((o, i) => o.id = i + 1);
+    return flights;
 }
 
 const getCheapFlights = () => {
